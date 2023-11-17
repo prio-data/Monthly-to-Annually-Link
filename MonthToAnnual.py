7e59d793d4b40bd6627aba9fa30372f39bc7bb73
nskazmi <142994595+nskazmi@users.noreply.github.com>
2023-11-17 14:24:46 +0100

    added predictions dictionary, updated December filtering in the ols_regression function

--- MonthToAnnual.py
+++ MonthToAnnual.py
@@ -11,47 +11,69 @@ import subprocess
 class MonthToAnnualRegression:
     def __init__(self, data):
         self.data = data
+        self.predictions = {}
         self.models = {}  # Dictionary to store regression models
         self.results = {}  # Dictionary to store regression results
 
 
     def ols_regression(self, x_columns, y_column,model_name):
-        X = self.data[x_columns]
+        filtered_rows = self.data.index.get_level_values('month_id') % 12 == 0
+
+        # Filter X and y Series based on the condition
+        X = self.data.loc[filtered_rows, x_columns]        
         X = sm.add_constant(X)  # Add a constant term for the intercept
-        y = self.data[y_column]
+        y = self.data.loc[filtered_rows, y_column]
         model = sm.OLS(y, X).fit()
         self.models[model_name] = model
         self.results[model_name] = model.summary()
         
-    def prediction(self, x_columns, y_column, model_name, country_id):
-        country_data = self.data[(self.data['country_id'] == country_id)]
-        # country_data = country_data.set_index('month_id').sort_index()
-        X = country_data[x_columns]
-        y = country_data[y_column]
-        print(X)
-        model = self.models[model_name]
-        print(model)
-        y_pred = model.predict(sm.add_constant(X))
-        return y_pred
       
 
+    # def plot_time_series_regression(self, x_columns, y_column, model_name, country_id):
+    #     country_data = self.data[(self.data['country_id'] == country_id)]
+    #     #country_data = country_data.set_index('month_id').sort_index()
+    #     X = country_data[x_columns]
+    #     y = country_data[y_column]
+    #     model = self.models[model_name]
+    #     y_pred = model.predict(sm.add_constant(X))
+    #     #plt.plot(y)
+    #     #plt.plot(y_pred)
+        
+    #     df1 = y_pred.reset_index()
+    #     df1.columns = ['month_id', 'country_id', 'value']
+    #     df2 = y.reset_index()
+    #     df2.columns = ['month_id', 'country_id', 'value']
+    #     # Plotting
+    #     plt.plot(df1['month_id'], df1['value'], marker='o',label='Predicted')
+    #     plt.plot(df2['month_id'], df2['value'], marker='o',label='Actual')
+
+    #     plt.xlabel('Month ID')
+    #     plt.ylabel('Infant Mortality')
+    #     plt.title('Plotting Series with MultiIndex')
+    #     plt.legend()
+
+    #     plt.show()
+    
+    
+    
     def plot_time_series_regression(self, x_columns, y_column, model_name, country_id):
         country_data = self.data[(self.data['country_id'] == country_id)]
-        #country_data = country_data.set_index('month_id').sort_index()
-        X = country_data[x_columns]
+        # country_data = country_data.set_index('month_id').sort_index()
+        x = country_data[x_columns]
         y = country_data[y_column]
-        model = self.models[model_name]
-        y_pred = model.predict(sm.add_constant(X))
+        #model = self.models[model_name]
+        y_pred = self.predictions[model_name][self.predictions[model_name].index.get_level_values('country_id') == country_id]
         #plt.plot(y)
         #plt.plot(y_pred)
-        
+        print(y)
+        print(y_pred)
         df1 = y_pred.reset_index()
         df1.columns = ['month_id', 'country_id', 'value']
         df2 = y.reset_index()
         df2.columns = ['month_id', 'country_id', 'value']
         # Plotting
-        plt.plot(df1['month_id'], df1['value'], marker='o',label='Predicted')
-        plt.plot(df2['month_id'], df2['value'], marker='o',label='Actual')
+        plt.plot(df1['month_id'], df1['value'], marker='o', label='Predicted')
+        plt.plot(df2['month_id'], df2['value'], marker='o', label='Actual')
 
         plt.xlabel('Month ID')
         plt.ylabel('Infant Mortality')
@@ -59,7 +81,7 @@ class MonthToAnnualRegression:
         plt.legend()
 
         plt.show()
-
+        
     def random_effects_regression(self, x_columns, y_column, group_column, model_name):
         X = self.data[x_columns]
         X = sm.add_constant(X)  # Add a constant term for the intercept
@@ -75,6 +97,13 @@ class MonthToAnnualRegression:
     def get_regression_matrix(self, model_name):
         return self.results.get(model_name, None)
 
+    def prediction(self, x_columns, y_column, model_name):
+        X = self.data[x_columns]
+        y = self.data[y_column]
+        model = self.models[model_name]
+        y_pred = model.predict(sm.add_constant(X))
+        self.predictions[model_name] = y_pred
+        
     def plot_regression(self, x_columns, y_column, model_name):
         X = self.data[x_columns]
         y = self.data[y_column]
