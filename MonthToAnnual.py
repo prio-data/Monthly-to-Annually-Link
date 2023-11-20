0fde514a900bcdfb87083fd4a55aba1f1c7b21ac
nskazmi <142994595+nskazmi@users.noreply.github.com>
2023-11-20 11:41:55 +0100

    initialized empty class

--- MonthToAnnual.py
+++ MonthToAnnual.py
@@ -9,123 +9,32 @@ import subprocess
 
 
 class MonthToAnnualRegression:
-    def __init__(self, data):
-        self.data = data
-        self.predictions = {}
-        self.models = {}  # Dictionary to store regression models
-        self.results = {}  # Dictionary to store regression results
+    def __init__(self, independent_variable, dependent_variable, model_type):
+        self.independent_variable = independent_variable
+        self.dependent_variable = dependent_variable
+        self.model_type = model_type
+        self.model = None
+        self.predicted_annual = None
 
+    def fit(self):
+        # Your fitting logic here, depending on the model_type
+        # For example, using statsmodels or scikit-learn to fit the model
+        # Example:
+        # self.model = YourModel.fit(self.independent_variable, self.dependent_variable)
+        pass  # Placeholder, replace with fitting logic
 
-    def ols_regression(self, x_columns, y_column,model_name):
-        filtered_rows = self.data.index.get_level_values('month_id') % 12 == 0
+    def predict_annual(self):
+        # Your logic to predict annual values based on the fitted model
+        # Store predictions in self.predicted_annual
+        pass  # Placeholder, replace with prediction logic
 
-        # Filter X and y Series based on the condition
-        X = self.data.loc[filtered_rows, x_columns]        
-        X = sm.add_constant(X)  # Add a constant term for the intercept
-        y = self.data.loc[filtered_rows, y_column]
-        model = sm.OLS(y, X).fit()
-        self.models[model_name] = model
-        self.results[model_name] = model.summary()
-        
-      
+    def predict(self, new_data):
+        # Your logic to predict values for new data using the fitted model
+        # Example:
+        # return self.model.predict(new_data)
+        pass  # Placeholder, replace with prediction logic
 
-    # def plot_time_series_regression(self, x_columns, y_column, model_name, country_id):
-    #     country_data = self.data[(self.data['country_id'] == country_id)]
-    #     #country_data = country_data.set_index('month_id').sort_index()
-    #     X = country_data[x_columns]
-    #     y = country_data[y_column]
-    #     model = self.models[model_name]
-    #     y_pred = model.predict(sm.add_constant(X))
-    #     #plt.plot(y)
-    #     #plt.plot(y_pred)
-        
-    #     df1 = y_pred.reset_index()
-    #     df1.columns = ['month_id', 'country_id', 'value']
-    #     df2 = y.reset_index()
-    #     df2.columns = ['month_id', 'country_id', 'value']
-    #     # Plotting
-    #     plt.plot(df1['month_id'], df1['value'], marker='o',label='Predicted')
-    #     plt.plot(df2['month_id'], df2['value'], marker='o',label='Actual')
-
-    #     plt.xlabel('Month ID')
-    #     plt.ylabel('Infant Mortality')
-    #     plt.title('Plotting Series with MultiIndex')
-    #     plt.legend()
-
-    #     plt.show()
-    
-    
-    
-    def plot_time_series_regression(self, x_columns, y_column, model_name, country_id):
-        country_data = self.data[(self.data['country_id'] == country_id)]
-        # country_data = country_data.set_index('month_id').sort_index()
-        x = country_data[x_columns]
-        y = country_data[y_column]
-        #model = self.models[model_name]
-        y_pred = self.predictions[model_name][self.predictions[model_name].index.get_level_values('country_id') == country_id]
-        #plt.plot(y)
-        #plt.plot(y_pred)
-        print(y)
-        print(y_pred)
-        df1 = y_pred.reset_index()
-        df1.columns = ['month_id', 'country_id', 'value']
-        df2 = y.reset_index()
-        df2.columns = ['month_id', 'country_id', 'value']
-        # Plotting
-        plt.plot(df1['month_id'], df1['value'], marker='o', label='Predicted')
-        plt.plot(df2['month_id'], df2['value'], marker='o', label='Actual')
-
-        plt.xlabel('Month ID')
-        plt.ylabel('Infant Mortality')
-        plt.title('Plotting Series with MultiIndex')
-        plt.legend()
-
-        plt.show()
-        
-    def random_effects_regression(self, x_columns, y_column, group_column, model_name):
-        X = self.data[x_columns]
-        X = sm.add_constant(X)  # Add a constant term for the intercept
-        y = self.data[y_column]
-        groups = self.data[group_column]
-
-        model = MixedLM(y, X, groups)
-        result = model.fit()
-        
-        self.models[model_name] = result
-        self.results[model_name] = result.summary()
-
-    def get_regression_matrix(self, model_name):
-        return self.results.get(model_name, None)
-
-    def prediction(self, x_columns, y_column, model_name):
-        X = self.data[x_columns]
-        y = self.data[y_column]
-        model = self.models[model_name]
-        y_pred = model.predict(sm.add_constant(X))
-        self.predictions[model_name] = y_pred
-        
-    def plot_regression(self, x_columns, y_column, model_name):
-        X = self.data[x_columns]
-        y = self.data[y_column]
-        model = self.models[model_name]
-        y_pred = model.predict(sm.add_constant(X))
-        
-        plt.scatter(y, y_pred)
-        plt.xlabel('Actual Values')
-        plt.ylabel('Predicted Values')
-        plt.title(f'{model_name}: Actual vs. Predicted')
-        plt.show()
-
-    @staticmethod
-    def f_test(model1, model2):
-        # f_value = (model1.ssr - model2.ssr) / (model2.df_model - model1.df_model) / (model2.ssr / model2.df_resid)
-        # p_value = 1 - stats.f.cdf(f_value, model2.df_model - model1.df_model, model2.df_resid)
-        # return f_value, p_value
-    
-        ftest = model1.compare_f_test(model2)
-        print(ftest)
-    
-    @staticmethod  
-    def countries_with_missing_data(df,country_id):
-        return len(df.query(f'country_id == {country_id}'))
+    def plot_predictions(self):
+        # Your logic to plot predictions
+        pass  # Placeholder, replace with plotting logic
 
--- README.md
+++
@@ -1,3 +0,0 @@
-# Monthly-to-Annually-Link
-Write prototype code to estimate the impact of conflict, measured monthly in VIEWS, on various outcomes that are measured annually, based on model sketched by Mike and Håvard
-
