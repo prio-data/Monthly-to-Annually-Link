bc9690e537209d6f6a10de9b36ebc49fd6b35631
nskazmi <142994595+nskazmi@users.noreply.github.com>
2023-11-24 13:40:50 +0100

    added dummies for fixed effect

--- MonthToAnnual.py
+++ MonthToAnnual.py
@@ -15,56 +15,48 @@ class MonthToAnnualRegression:
         self.models = {}  # Dictionary to store regression models
         self.results = {}  # Dictionary to store regression results
 
-
-    def ols_regression(self, x_columns, y_column,model_name):
-        filtered_rows = self.data.index.get_level_values('month_id') % 12 == 0
+    def ols_regression(self, x_columns, y_column, model_name):
+        filtered_rows = self.data.index.get_level_values('month_id') % 1 == 0
 
         # Filter X and y Series based on the condition
-        X = self.data.loc[filtered_rows, x_columns]        
-        X = sm.add_constant(X)  # Add a constant term for the intercept
+        X = self.data.loc[filtered_rows, x_columns]
+        dummy_vars = pd.get_dummies(
+            self.data.loc[filtered_rows, 'country_id'], prefix='country', drop_first=True, dtype=int)
+        X = pd.concat([X, dummy_vars], axis=1)
+        # X = sm.add_constant(X)  # Add a constant term for the intercept
+        print(list(X.columns))
         y = self.data.loc[filtered_rows, y_column]
+        print(y)
+
         model = sm.OLS(y, X).fit()
         self.models[model_name] = model
         self.results[model_name] = model.summary()
-        
-      
 
-    # def plot_time_series_regression(self, x_columns, y_column, model_name, country_id):
-    #     country_data = self.data[(self.data['country_id'] == country_id)]
-    #     #country_data = country_data.set_index('month_id').sort_index()
-    #     X = country_data[x_columns]
-    #     y = country_data[y_column]
-    #     model = self.models[model_name]
-    #     y_pred = model.predict(sm.add_constant(X))
-    #     #plt.plot(y)
-    #     #plt.plot(y_pred)
-        
-    #     df1 = y_pred.reset_index()
-    #     df1.columns = ['month_id', 'country_id', 'value']
-    #     df2 = y.reset_index()
-    #     df2.columns = ['month_id', 'country_id', 'value']
-    #     # Plotting
-    #     plt.plot(df1['month_id'], df1['value'], marker='o',label='Predicted')
-    #     plt.plot(df2['month_id'], df2['value'], marker='o',label='Actual')
+    def prediction(self, x_columns, y_column, model_name):
+        filtered_rows = self.data.index.get_level_values('month_id') % 1 == 0
 
-    #     plt.xlabel('Month ID')
-    #     plt.ylabel('Infant Mortality')
-    #     plt.title('Plotting Series with MultiIndex')
-    #     plt.legend()
+        X = self.data.loc[filtered_rows, x_columns]
+        dummy_vars = pd.get_dummies(
+            self.data.loc[filtered_rows, 'country_id'], prefix='country', drop_first=True, dtype=int)
+        X = pd.concat([X, dummy_vars], axis=1)
+        # X = sm.add_constant(X)  # Add a constant term for the intercept
+        print(list(X.columns))
+        y = self.data.loc[filtered_rows, y_column]
 
-    #     plt.show()
-    
-    
-    
+        model = self.models[model_name]
+        y_pred = model.predict(X)
+        self.predictions[model_name] = y_pred
+
     def plot_time_series_regression(self, x_columns, y_column, model_name, country_id, shock_variable):
         country_data = self.data[(self.data['country_id'] == country_id)]
         # country_data = country_data.set_index('month_id').sort_index()
         x = country_data[x_columns]
         y = country_data[y_column]
-        #model = self.models[model_name]
-        y_pred = self.predictions[model_name][self.predictions[model_name].index.get_level_values('country_id') == country_id]
-        #plt.plot(y)
-        #plt.plot(y_pred)
+        # model = self.models[model_name]
+        y_pred = self.predictions[model_name][self.predictions[model_name].index.get_level_values(
+            'country_id') == country_id]
+        # plt.plot(y)
+        # plt.plot(y_pred)
         print(y)
         print(y_pred)
         df1 = y_pred.reset_index()
@@ -74,7 +66,8 @@ class MonthToAnnualRegression:
         # Plotting
         plt.plot(df1['month_id'], df1['value'], marker='o', label='Predicted')
         plt.plot(df2['month_id'], df2['value'], marker='o', label='Actual')
-        plt.plot(df2['month_id'], x[shock_variable], marker='o', label=shock_variable)
+        plt.plot(df2['month_id'], x[shock_variable],
+                 marker='o', label=shock_variable)
 
         plt.xlabel('Month ID')
         plt.ylabel('Infant Mortality')
@@ -82,35 +75,18 @@ class MonthToAnnualRegression:
         plt.legend()
 
         plt.show()
-        
-    def random_effects_regression(self, x_columns, y_column, group_column, model_name):
-        X = self.data[x_columns]
-        X = sm.add_constant(X)  # Add a constant term for the intercept
-        y = self.data[y_column]
-        groups = self.data[group_column]
 
-        model = MixedLM(y, X, groups)
-        result = model.fit()
-        
-        self.models[model_name] = result
-        self.results[model_name] = result.summary()
-
-    def get_regression_matrix(self, model_name):
-        return self.results.get(model_name, None)
-
-    def prediction(self, x_columns, y_column, model_name):
-        X = self.data[x_columns]
-        y = self.data[y_column]
-        model = self.models[model_name]
-        y_pred = model.predict(sm.add_constant(X))
-        self.predictions[model_name] = y_pred
-        
     def plot_regression(self, x_columns, y_column, model_name):
+        filtered_rows = self.data.index.get_level_values('month_id') % 1 == 0
         X = self.data[x_columns]
         y = self.data[y_column]
+        dummy_vars = pd.get_dummies(
+            self.data.loc[filtered_rows, 'country_id'], prefix='country', drop_first=True, dtype=int)
+        X = pd.concat([X, dummy_vars], axis=1)
+        print(X)
         model = self.models[model_name]
         y_pred = model.predict(sm.add_constant(X))
-        
+
         plt.scatter(y, y_pred)
         plt.xlabel('Actual Values')
         plt.ylabel('Predicted Values')
@@ -122,11 +98,10 @@ class MonthToAnnualRegression:
         # f_value = (model1.ssr - model2.ssr) / (model2.df_model - model1.df_model) / (model2.ssr / model2.df_resid)
         # p_value = 1 - stats.f.cdf(f_value, model2.df_model - model1.df_model, model2.df_resid)
         # return f_value, p_value
-    
+
         ftest = model1.compare_f_test(model2)
         print(ftest)
-    
-    @staticmethod  
-    def countries_with_missing_data(df,country_id):
-        return len(df.query(f'country_id == {country_id}'))
 
+    @staticmethod
+    def countries_with_missing_data(df, country_id):
+        return len(df.query(f'country_id == {country_id}'))
