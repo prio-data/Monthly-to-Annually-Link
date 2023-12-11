97b9872eb6441cb036bc68c5552c241ab94fa5f3
nskazmi <142994595+nskazmi@users.noreply.github.com>
2023-12-11 14:32:26 +0100

    using HurdleRegression template to create a scikit estimator

--- theClass.py
+++ theClass.py
@@ -1,74 +1,102 @@
-from sklearn.base import BaseEstimator, RegressorMixin
-from sklearn.utils.validation import check_X_y
-import pandas as pd
+from typing import Optional, Union
 import numpy as np
-import statsmodels.api as sm
-from statsmodels.regression.mixed_linear_model import MixedLM
-import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.base import BaseEstimator
+from sklearn.utils.estimator_checks import check_estimator
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.ensemble import HistGradientBoostingClassifier
+from xgboost import XGBRegressor
+from xgboost import XGBClassifier
+from xgboost import XGBRFRegressor, XGBRFClassifier
+from lightgbm import LGBMClassifier, LGBMRegressor
+
+# from lightgbm import LGBMClassifier, LGBMRegressor
 
 
-class Regressor(BaseEstimator, RegressorMixin):
-    '''
-    This class is a very specific class for the project Month to Annual
-    It has functions for OLS and MixedLM from statsmodels
-    This class inherits from scikit-learn BaseEstimator
-    It creates a regression model based on December months only from VIEWS data
-    But it can be used to predict the outcomes for every month
-    '''
+class MotanRegression(BaseEstimator):
+    """ 
+    Regression model to fit using December data and predict on all monthly data
+    Implementeted as a valid sklearn estimator, so it can be used in pipelines and GridSearch objects.
+    Args:
+        reg_name: currently supports either 'linear'
+        reg_params: dict of parameters to pass to regression sub-model when initialized
+    """
 
-    def __init__(self, use_mixed_effects=False, groups=None):
-        self.model = None
-        self.use_mixed_effects = use_mixed_effects
-        if groups is not None:
-            self.groups = groups.loc[groups.index.get_level_values(
-                'month_id') % 12 == 0]
-        else:
-            self.groups = groups
+    def __init__(self,
+                 reg_name: str = 'linear',
+                 reg_params: Optional[dict] = None):
 
-    def fit(self, X, y):
+        self.reg_name = reg_name
+        self.reg_params = reg_params
+        # self.reg_fi = []
 
-        # X, y = check_X_y(X, y, y_numeric=True)
-        if not isinstance(X, pd.DataFrame):
-            raise ValueError("Input X should be a pandas DataFrame.")
-        X = X.query('month_id % 12 == 0')
-        y = y.loc[y.index.get_level_values(
-            'month_id') % 12 == 0]
-        if self.use_mixed_effects:
-            if self.groups is None:
-                raise ValueError(
-                    "Groups should be provided for mixed-effects modeling.")
-            X = sm.add_constant(X)
-            self.model = MixedLM(y, X, groups=self.groups).fit()
-        else:
+    @staticmethod
+    def _resolve_estimator(func_name: str):
+        """ Lookup table for supported estimators.
+        This is necessary because sklearn estimator default arguments
+        must pass equality test, and instantiated sub-estimators are not equal. """
 
-            X = sm.add_constant(X)
-            self.model = sm.OLS(y, X).fit()
+        funcs = {'linear': LinearRegression(),
+                 'logistic': LogisticRegression(solver='liblinear'),
+                 'LGBMRegressor': LGBMRegressor(n_estimators=250),
+                 'LGBMClassifier': LGBMClassifier(n_estimators=250),
+                 'RFRegressor': XGBRFRegressor(n_estimators=250, n_jobs=-2),
+                 'RFClassifier': XGBRFClassifier(n_estimators=250, n_jobs=-2),
+                 'GBMRegressor': GradientBoostingRegressor(n_estimators=200),
+                 'GBMClassifier': GradientBoostingClassifier(n_estimators=200),
+                 'XGBRegressor': XGBRegressor(n_estimators=100, learning_rate=0.05, n_jobs=-2),
+                 'XGBClassifier': XGBClassifier(n_estimators=100, learning_rate=0.05, n_jobs=-2),
+                 'HGBRegressor': HistGradientBoostingRegressor(max_iter=200),
+                 'HGBClassifier': HistGradientBoostingClassifier(max_iter=200),
+                 }
+
+        return funcs[func_name]
+
+    def fit(self,
+            X: Union[np.ndarray, pd.DataFrame],
+            y: Union[np.ndarray, pd.Series]):
+        X, y = check_X_y(X, y, dtype=None,
+                         accept_sparse=False,
+                         accept_large_sparse=False,
+                         force_all_finite='allow-nan')
+
+        if X.shape[1] < 2:
+            raise ValueError('Cannot fit model when n_features = 1')
+
+        if np.isnan(X).any() or np.isnan(y).any() or np.isinf(X).any() or np.isinf(y).any():
+            raise ValueError("Input data contains NaN or inf values.")
+
+        self.reg_ = self._resolve_estimator(self.reg_name)  # regression model
+        if self.reg_params:
+            self.reg_.set_params(**self.reg_params)  # regression parameters
+        self.reg_.fit(X[y > 0], y[y > 0])
+        # self.reg_fi = self.reg_.feature_importances_
+
+        self.is_fitted_ = True
         return self
 
-    def predict(self, X):
-        if not isinstance(X, pd.DataFrame):
-            raise ValueError("Input X should be a pandas DataFrame.")
+    def predict(self, X: Union[np.ndarray, pd.DataFrame]):
+        X = check_array(X, accept_sparse=False, accept_large_sparse=False)
+        check_is_fitted(self, 'is_fitted_')
+        return self.reg_.predict(X)
 
-        X_const = sm.add_constant(X)
-        predictions = self.model.predict(X_const)
 
-        # Adding the indexes to the predictions
-        predictions_with_indexes = pd.DataFrame(
-            predictions, index=X.index, columns=["Predictions"])
-        predictions_with_indexes.index.name = X.index.name
+def manual_test():
+    reg = MotanRegression()
+    # check_estimator(reg) # uncomment if we need to check requirements for scikit-learn estimator
+    from sklearn.datasets import make_regression
+    X, y = make_regression()
+    reg = MotanRegression()
+    reg.fit(X, y)
+    print(reg.predict(X))
+    print('Done')
 
-        return predictions_with_indexes
 
-    def predict_annual(self, X):
-        if not isinstance(X, pd.DataFrame):
-            raise ValueError("Input X should be a pandas DataFrame.")
-
-        X_const = sm.add_constant(X.query('country_id % 12 == 0'))
-        predictions = self.model.predict(X_const)
-
-        # Adding the indexes to the predictions
-        predictions_with_indexes = pd.DataFrame(
-            predictions, index=X.index, columns=["Annual Predictions"])
-        predictions_with_indexes.index.name = X.index.name
-
-        return predictions_with_indexes
+if __name__ == '__main__':
+    manual_test()
